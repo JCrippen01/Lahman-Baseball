fecd0a563154bffb01a519b7de6c08c4ec583c51
CelineIT <celinedamoreit@gmail.com>
2021-02-18 21:19:50 -0500

    excel chart to show correlation q11

--- question_11.sql
+++ question_11.sql
@@ -15,13 +15,18 @@ JOIN teams AS t ON t.teamid = salaries.teamid AND t.yearid = salaries.yearid
 WHERE t.yearid >= '2000'
 GROUP BY  t.teamid, t.yearid
 ORDER BY t.yearid, t.teamid
+-- this works
 
 -- now adding the wins
 
-SELECT t.yearid, t.teamid, t.w AS win, sum(salary::decimal::money) AS salary
+SELECT t.yearid, t.teamid, t.w as win, sum(salary::decimal::money) AS salary
 FROM salaries
 JOIN teams AS t ON t.teamid = salaries.teamid AND t.yearid = salaries.yearid
 WHERE t.yearid >= '2000'
-GROUP BY  t.teamid, t.yearid, t.w
-ORDER BY t.yearid, t.teamid
+GROUP BY  t.teamid, t.yearid, win
+ORDER BY t.teamid, t.yearid;
+-- this code is correct, I checked the results online
+
+
+
 
