fc91336f9217b2b7ff59d6ebec0cc51a6bced0f3
CelineIT <celinedamoreit@gmail.com>
2021-02-18 23:06:28 -0500

    q10 almost done

--- question_10.sql
+++ question_10.sql
@@ -50,11 +50,27 @@ AND wswin = 'Y'
 GROUP BY s.schoolname, player_name, teams.wswin, cp.schoolid, cp.yearid
 ORDER BY s.schoolname;
 
-SELECT s.schoolname, CONCAT(namefirst, ' ', namelast) as player_name, teams.wswin, cp.schoolid, cp.yearid, count(schoolname) as college_win
+-- FINAL code showing how many times a school won in the WorldSeries
+WITH cte_wins as
+	(SELECT DISTINCT s.schoolname, CONCAT(namefirst, ' ', namelast) as player_name, teams.wswin, cp.yearid,
+COUNT (schoolname) OVER (PARTITION BY schoolname) as Schoolnamewin
 FROM schools as s	INNER JOIN collegeplaying as cp ON cp.schoolid = s.schoolid
 					INNER JOIN teams ON teams.yearid = cp.yearid
 					INNER JOIN people ON people.playerid = cp.playerid
 WHERE s.schoolstate = 'TN'
-AND wswin = 'Y'
-GROUP BY s.schoolname, player_name, teams.wswin, cp.schoolid, cp.yearid
-ORDER BY s.schoolname;
+AND wswin = 'Y')
+SELECT  *
+FROM cte_wins
+ORDER BY schoolnamewin DESC;
+
+WITH cte_wins as
+	(SELECT DISTINCT s.schoolname, CONCAT(namefirst, ' ', namelast) as player_name, teams.wswin, cp.yearid,
+COUNT (schoolname) OVER (PARTITION BY schoolname) as Schoolnamewin
+FROM schools as s	INNER JOIN collegeplaying as cp ON cp.schoolid = s.schoolid
+					INNER JOIN teams ON teams.yearid = cp.yearid
+					INNER JOIN people ON people.playerid = cp.playerid
+WHERE s.schoolstate = 'TN'
+AND wswin = 'Y')
+SELECT  DISTINCT schoolname, schoolnamewin
+FROM cte_wins
+ORDER BY schoolnamewin DESC;
